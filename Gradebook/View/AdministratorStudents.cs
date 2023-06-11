ba979e47e239820f547a99daa4226ff9b8109cd0
Jennifer Parks <jp00213@my.westga.edu>
2023-06-11 10:23:48 -0400

    Added comments and removed usings

--- Gradebook/View/AdministratorStudents.cs
+++ Gradebook/View/AdministratorStudents.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace Gradebook.View
 {
+    /// <summary>
+    /// Administrator sub-dashboard for students
+    /// </summary>
     public partial class AdministratorStudents : Form
     {
+        /// <summary>
+        /// Constructor for the Administrator Student Form
+        /// </summary>
         public AdministratorStudents()
         {
             InitializeComponent();
