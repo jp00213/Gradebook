0e668bf1e5c39f1cecb2ba3e52efc780e08d8eb7
Alyssa Harris <ah00214@my.westga.edu>
2023-06-13 10:12:02 -0400

    Changed font size

--- Gradebook/View/AdministratorTeachers.Designer.cs
+++ Gradebook/View/AdministratorTeachers.Designer.cs
@@ -41,6 +41,7 @@
             this.adminTeacherTabControl.Controls.Add(this.addTeacherTabPage);
             this.adminTeacherTabControl.Controls.Add(this.viewTeacherTabPage);
             this.adminTeacherTabControl.Controls.Add(this.updateTeacherTabPage);
+            this.adminTeacherTabControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.adminTeacherTabControl.Location = new System.Drawing.Point(-4, 61);
             this.adminTeacherTabControl.Name = "adminTeacherTabControl";
             this.adminTeacherTabControl.SelectedIndex = 0;
@@ -49,33 +50,33 @@
             // 
             // addTeacherTabPage
             // 
-            this.addTeacherTabPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.addTeacherTabPage.Location = new System.Drawing.Point(4, 22);
+            this.addTeacherTabPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addTeacherTabPage.Location = new System.Drawing.Point(4, 29);
             this.addTeacherTabPage.Name = "addTeacherTabPage";
             this.addTeacherTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.addTeacherTabPage.Size = new System.Drawing.Size(987, 539);
+            this.addTeacherTabPage.Size = new System.Drawing.Size(987, 532);
             this.addTeacherTabPage.TabIndex = 0;
             this.addTeacherTabPage.Text = "Add Teacher";
             this.addTeacherTabPage.UseVisualStyleBackColor = true;
             // 
             // viewTeacherTabPage
             // 
-            this.viewTeacherTabPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.viewTeacherTabPage.Location = new System.Drawing.Point(4, 22);
+            this.viewTeacherTabPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewTeacherTabPage.Location = new System.Drawing.Point(4, 29);
             this.viewTeacherTabPage.Name = "viewTeacherTabPage";
             this.viewTeacherTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.viewTeacherTabPage.Size = new System.Drawing.Size(987, 539);
+            this.viewTeacherTabPage.Size = new System.Drawing.Size(987, 532);
             this.viewTeacherTabPage.TabIndex = 1;
             this.viewTeacherTabPage.Text = "View Teacher";
             this.viewTeacherTabPage.UseVisualStyleBackColor = true;
             // 
             // updateTeacherTabPage
             // 
-            this.updateTeacherTabPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.updateTeacherTabPage.Location = new System.Drawing.Point(4, 22);
+            this.updateTeacherTabPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateTeacherTabPage.Location = new System.Drawing.Point(4, 29);
             this.updateTeacherTabPage.Name = "updateTeacherTabPage";
             this.updateTeacherTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.updateTeacherTabPage.Size = new System.Drawing.Size(987, 539);
+            this.updateTeacherTabPage.Size = new System.Drawing.Size(987, 532);
             this.updateTeacherTabPage.TabIndex = 2;
             this.updateTeacherTabPage.Text = "Update Teacher";
             this.updateTeacherTabPage.UseVisualStyleBackColor = true;
@@ -83,9 +84,10 @@
             // backHomeButton
             // 
             this.backHomeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backHomeButton.ForeColor = System.Drawing.SystemColors.ControlText;
             this.backHomeButton.Location = new System.Drawing.Point(22, 13);
             this.backHomeButton.Name = "backHomeButton";
-            this.backHomeButton.Size = new System.Drawing.Size(75, 30);
+            this.backHomeButton.Size = new System.Drawing.Size(104, 30);
             this.backHomeButton.TabIndex = 1;
             this.backHomeButton.Text = "Home";
             this.backHomeButton.UseVisualStyleBackColor = true;
