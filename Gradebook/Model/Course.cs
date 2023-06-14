dd78a5ab771737c35c0192ab49a6f80756185f1a
Jennifer Parks <jp00213@my.westga.edu>
2023-06-14 17:53:22 -0400

    Creates Course model and tests, updates database.

---
+++ Gradebook/Model/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradebook.Model
+{
+    /// <summary>
+    /// Course model class
+    /// </summary>
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Name { get; set; }
+        public string Prefix { get; set; }
+        public string Number { get; set; }
+        public int Section { get; set; }
+        public int CreditHours { get; set; }
+        public string Semester { get; set; }
+        public int Year { get; set; }
+        public int TeacherID { get; set; }
+    }
+}
---
+++ GradebookTest/Model/CourseConstructor.cs
@@ -0,0 +1,40 @@
+﻿using Gradebook.Model;
+
+namespace GradebookTest.Model
+{
+    public class CourseConstructor
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ShouldCreateCourse()
+        {
+            var testCourse = new Course
+            {
+                Name = "Test Name",
+                Prefix = "EDU",
+                Number = "1234",
+                Section = 23,
+                CreditHours = 3,
+                Semester = "summer",
+                Year = 2023
+            };
+
+            Assert.That(testCourse.Name, Is.EqualTo("Test Name"));
+            Assert.That(testCourse.Prefix, Is.EqualTo("EDU"));
+            Assert.That(testCourse.Number, Is.EqualTo("1234"));
+            Assert.That(testCourse.Section, Is.EqualTo(23));
+            Assert.That(testCourse.CreditHours, Is.EqualTo(3));
+            Assert.That(testCourse.Semester, Is.EqualTo("summer"));
+            Assert.That(testCourse.Year, Is.EqualTo(2023));
+            Assert.That(testCourse.TeacherID, Is.EqualTo(0));
+
+            testCourse.TeacherID = 29;
+            Assert.That(testCourse.TeacherID, Is.EqualTo(29));
+        }
+
+    }
+}
