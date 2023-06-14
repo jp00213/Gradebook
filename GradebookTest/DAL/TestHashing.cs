e90991f8146871f2aa8277497436c090b99be414
Jennifer Parks <jp00213@my.westga.edu>
2023-06-14 17:53:27 -0400

    Tests Hashing method.

---
+++ GradebookTest/DAL/TestHashing.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gradebook.DAL;
+
+namespace GradebookTest.DAL
+{
+    public class TestHashing
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ShouldHashPasswordTheSameWayEachTime()
+        {
+            var bytes = new UTF8Encoding().GetBytes("testing");
+            byte[] hashBytes;
+            using (var algorithm = new System.Security.Cryptography.SHA512Managed())
+            {
+                hashBytes = algorithm.ComputeHash(bytes);
+            }
+            Assert.That(Convert.ToBase64String(hashBytes), Is.EqualTo(Hashing.HashPassword("testing")));
+        }
+
+
+    }
+}
