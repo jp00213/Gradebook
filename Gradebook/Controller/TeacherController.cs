da93b9b03b8d675674635117f0a7b0eda76030f5
Alyssa Harris <ah00214@my.westga.edu>
2023-06-15 12:49:04 -0400

    Added code to create a person as a teacher

--- Gradebook/Controller/TeacherController.cs
+++ Gradebook/Controller/TeacherController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Gradebook.DAL;
+using Gradebook.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,30 @@ using System.Threading.Tasks;
 
 namespace Gradebook.Controller
 {
-    internal class TeacherController
+    public class TeacherController
     {
+        /// <summary>
+        /// The controller class that is the go-between for the PersonDAL and the view
+        /// </summary>
+        /// 
+        private readonly PersonDAL _personDAL;
+
+        /// <summary>
+        /// Creates a teacherController object
+        /// </summary>
+        public TeacherController()
+        {
+            _personDAL = new PersonDAL();
+        }
+
+        /// <summary>
+        /// Adds a person as a teacher.
+        /// </summary>
+        /// <param name="person"></param>
+        /// <returns></returns>
+        public Boolean AddPersonAsTeacher(Person person)
+        {
+            return this._personDAL.AddPersonAsTeacher(person);
+        }
     }
 }
