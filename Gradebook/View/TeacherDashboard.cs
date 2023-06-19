38f969c27a83c494c7504ae2003b17990c6bf9f0
Jennifer Parks <jp00213@my.westga.edu>
2023-06-18 20:36:42 -0400

    Adds login functions for students and teachers to plain dashboard

---
+++ Gradebook/View/TeacherDashboard.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Forms;
+
+namespace Gradebook.View
+{
+    /// <summary>
+    /// Main Dashboard for Teachers
+    /// </summary>
+    public partial class TeacherDashboard : Form
+    {
+        /// <summary>
+        /// Constructor for the Main Dashboard for Teachers
+        /// </summary>
+        /// <param name="username"></param>
+        public TeacherDashboard(string username)
+        {
+            InitializeComponent();
+        }
+
+        private void logoutLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            Owner.Show();
+        }
+
+        private void AdministratorDashboard_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
