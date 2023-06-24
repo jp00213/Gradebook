ceaf4f108563c03d8fb2852176f5ca66dae87dd7
Alyssa Harris <ah00214@my.westga.edu>
2023-06-23 23:26:31 -0400

    Removed unneccesary usings

--- Gradebook/UserControls/TeacherClassesUserControl.cs
+++ Gradebook/UserControls/TeacherClassesUserControl.cs
@@ -2,13 +2,6 @@
 using Gradebook.Model;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Gradebook.UserControls
