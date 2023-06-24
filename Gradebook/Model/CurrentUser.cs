706aeb79cd823e006e61853241d61dac684f2202
Alyssa Harris <ah00214@my.westga.edu>
2023-06-23 20:26:56 -0400

    Created Current User class

---
+++ Gradebook/Model/CurrentUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradebook.Model
+{
+    public class CurrentUser
+    {
+        public string userName { get; set; }
+
+        public CurrentUser() { }
+
+    }
+}
