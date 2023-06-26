e70e8bd4c3fbf7f561b3c2dfaf81a41a5d67872d
Alyssa Harris <ah00214@my.westga.edu>
2023-06-26 14:26:47 -0400

    Started wiring add assignment page

--- Gradebook/UserControls/TeacherAddAssignmentUserControl.Designer.cs
+++ Gradebook/UserControls/TeacherAddAssignmentUserControl.Designer.cs
@@ -28,24 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.selectClassLabel = new System.Windows.Forms.Label();
             this.selectClassComboBox = new System.Windows.Forms.ComboBox();
             this.assignmentTypeLabel = new System.Windows.Forms.Label();
             this.assignmentTypeComboBox = new System.Windows.Forms.ComboBox();
             this.classAssignmentsDataGridView = new System.Windows.Forms.DataGridView();
+            this.assignmentIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.courseIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.weightDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.assignmentBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.classAssignmentsLabel = new System.Windows.Forms.Label();
             this.weightLabel = new System.Windows.Forms.Label();
             this.descriptionLabel = new System.Windows.Forms.Label();
             this.descriptionRichTextBox = new System.Windows.Forms.RichTextBox();
-            this.weightNumericUpDown = new System.Windows.Forms.NumericUpDown();
             this.clearButton = new System.Windows.Forms.Button();
             this.saveButton = new System.Windows.Forms.Button();
             this.classErrorMessageLabel = new System.Windows.Forms.Label();
             this.weightErrorMessageLabel = new System.Windows.Forms.Label();
             this.assignmentTypeErrorMessageLabel = new System.Windows.Forms.Label();
             this.descriptionErrorMessageLabel = new System.Windows.Forms.Label();
+            this.weightTextBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.classAssignmentsDataGridView)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.weightNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assignmentBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // selectClassLabel
@@ -65,6 +71,7 @@
             this.selectClassComboBox.Name = "selectClassComboBox";
             this.selectClassComboBox.Size = new System.Drawing.Size(253, 21);
             this.selectClassComboBox.TabIndex = 1;
+            this.selectClassComboBox.SelectedIndexChanged += new System.EventHandler(this.ComboBox_SelectedIndexChanged);
             // 
             // assignmentTypeLabel
             // 
@@ -83,20 +90,56 @@
             this.assignmentTypeComboBox.Name = "assignmentTypeComboBox";
             this.assignmentTypeComboBox.Size = new System.Drawing.Size(253, 21);
             this.assignmentTypeComboBox.TabIndex = 2;
+            this.assignmentTypeComboBox.SelectedIndexChanged += new System.EventHandler(this.ComboBox_SelectedIndexChanged);
             // 
             // classAssignmentsDataGridView
             // 
+            this.classAssignmentsDataGridView.AutoGenerateColumns = false;
             this.classAssignmentsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.classAssignmentsDataGridView.Location = new System.Drawing.Point(126, 385);
+            this.classAssignmentsDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.assignmentIDDataGridViewTextBoxColumn,
+            this.courseIDDataGridViewTextBoxColumn,
+            this.descriptionDataGridViewTextBoxColumn,
+            this.weightDataGridViewTextBoxColumn});
+            this.classAssignmentsDataGridView.DataSource = this.assignmentBindingSource;
+            this.classAssignmentsDataGridView.Location = new System.Drawing.Point(128, 351);
             this.classAssignmentsDataGridView.Name = "classAssignmentsDataGridView";
             this.classAssignmentsDataGridView.Size = new System.Drawing.Size(756, 238);
             this.classAssignmentsDataGridView.TabIndex = 4;
+            // 
+            // assignmentIDDataGridViewTextBoxColumn
+            // 
+            this.assignmentIDDataGridViewTextBoxColumn.DataPropertyName = "AssignmentID";
+            this.assignmentIDDataGridViewTextBoxColumn.HeaderText = "AssignmentID";
+            this.assignmentIDDataGridViewTextBoxColumn.Name = "assignmentIDDataGridViewTextBoxColumn";
+            // 
+            // courseIDDataGridViewTextBoxColumn
+            // 
+            this.courseIDDataGridViewTextBoxColumn.DataPropertyName = "CourseID";
+            this.courseIDDataGridViewTextBoxColumn.HeaderText = "CourseID";
+            this.courseIDDataGridViewTextBoxColumn.Name = "courseIDDataGridViewTextBoxColumn";
+            // 
+            // descriptionDataGridViewTextBoxColumn
+            // 
+            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "Description";
+            this.descriptionDataGridViewTextBoxColumn.HeaderText = "Description";
+            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            // 
+            // weightDataGridViewTextBoxColumn
+            // 
+            this.weightDataGridViewTextBoxColumn.DataPropertyName = "Weight";
+            this.weightDataGridViewTextBoxColumn.HeaderText = "Weight";
+            this.weightDataGridViewTextBoxColumn.Name = "weightDataGridViewTextBoxColumn";
+            // 
+            // assignmentBindingSource
+            // 
+            this.assignmentBindingSource.DataSource = typeof(Gradebook.Model.Assignment);
             // 
             // classAssignmentsLabel
             // 
             this.classAssignmentsLabel.AutoSize = true;
             this.classAssignmentsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.classAssignmentsLabel.Location = new System.Drawing.Point(122, 352);
+            this.classAssignmentsLabel.Location = new System.Drawing.Point(124, 318);
             this.classAssignmentsLabel.Name = "classAssignmentsLabel";
             this.classAssignmentsLabel.Size = new System.Drawing.Size(161, 20);
             this.classAssignmentsLabel.TabIndex = 5;
@@ -129,13 +172,7 @@
             this.descriptionRichTextBox.Size = new System.Drawing.Size(249, 75);
             this.descriptionRichTextBox.TabIndex = 4;
             this.descriptionRichTextBox.Text = "";
-            // 
-            // weightNumericUpDown
-            // 
-            this.weightNumericUpDown.Location = new System.Drawing.Point(213, 118);
-            this.weightNumericUpDown.Name = "weightNumericUpDown";
-            this.weightNumericUpDown.Size = new System.Drawing.Size(253, 20);
-            this.weightNumericUpDown.TabIndex = 3;
+            this.descriptionRichTextBox.TextChanged += new System.EventHandler(this.ComboBox_SelectedIndexChanged);
             // 
             // clearButton
             // 
@@ -146,6 +183,7 @@
             this.clearButton.TabIndex = 6;
             this.clearButton.Text = "Clear Assignment";
             this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
             // 
             // saveButton
             // 
@@ -156,6 +194,7 @@
             this.saveButton.TabIndex = 5;
             this.saveButton.Text = "Save Assignment";
             this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
             // 
             // classErrorMessageLabel
             // 
@@ -189,17 +228,25 @@
             this.descriptionErrorMessageLabel.Size = new System.Drawing.Size(0, 13);
             this.descriptionErrorMessageLabel.TabIndex = 16;
             // 
+            // weightTextBox
+            // 
+            this.weightTextBox.Location = new System.Drawing.Point(216, 117);
+            this.weightTextBox.Name = "weightTextBox";
+            this.weightTextBox.Size = new System.Drawing.Size(253, 20);
+            this.weightTextBox.TabIndex = 17;
+            this.weightTextBox.TextChanged += new System.EventHandler(this.ComboBox_SelectedIndexChanged);
+            // 
             // TeacherAddAssignmentUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.weightTextBox);
             this.Controls.Add(this.descriptionErrorMessageLabel);
             this.Controls.Add(this.assignmentTypeErrorMessageLabel);
             this.Controls.Add(this.weightErrorMessageLabel);
             this.Controls.Add(this.classErrorMessageLabel);
             this.Controls.Add(this.saveButton);
             this.Controls.Add(this.clearButton);
-            this.Controls.Add(this.weightNumericUpDown);
             this.Controls.Add(this.descriptionRichTextBox);
             this.Controls.Add(this.descriptionLabel);
             this.Controls.Add(this.weightLabel);
@@ -212,7 +259,7 @@
             this.Name = "TeacherAddAssignmentUserControl";
             this.Size = new System.Drawing.Size(999, 636);
             ((System.ComponentModel.ISupportInitialize)(this.classAssignmentsDataGridView)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.weightNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assignmentBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -229,12 +276,17 @@
         private System.Windows.Forms.Label weightLabel;
         private System.Windows.Forms.Label descriptionLabel;
         private System.Windows.Forms.RichTextBox descriptionRichTextBox;
-        private System.Windows.Forms.NumericUpDown weightNumericUpDown;
         private System.Windows.Forms.Button clearButton;
         private System.Windows.Forms.Button saveButton;
         private System.Windows.Forms.Label classErrorMessageLabel;
         private System.Windows.Forms.Label weightErrorMessageLabel;
         private System.Windows.Forms.Label assignmentTypeErrorMessageLabel;
         private System.Windows.Forms.Label descriptionErrorMessageLabel;
+        private System.Windows.Forms.TextBox weightTextBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn assignmentIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn courseIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn weightDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource assignmentBindingSource;
     }
 }
--- Gradebook/UserControls/TeacherAddAssignmentUserControl.cs
+++ Gradebook/UserControls/TeacherAddAssignmentUserControl.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Gradebook.Controller;
+using Gradebook.Function;
+using Gradebook.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,21 +15,188 @@ namespace Gradebook.UserControls
 {
     public partial class TeacherAddAssignmentUserControl : UserControl
     {
+        private CourseController _courseController;
+        private TeacherController _teacherController;
+        private AssignmentController _assignmentController;
+        private int _courseID;
         public TeacherAddAssignmentUserControl()
         {
             InitializeComponent();
+            this._courseController = new CourseController();
+            this._teacherController= new TeacherController();
+            this._assignmentController= new AssignmentController();
             this.SetUpTypeComboBox();
+            this.SetUpClassComboBox();
         }
+
 
         private void SetUpTypeComboBox()
         {
-            this.assignmentTypeComboBox.Items.Insert(0, "---Select---");
             this.assignmentTypeComboBox.Items.Insert(0, "Quiz");
-            this.assignmentTypeComboBox.Items.Insert(0, "Test");
-            this.assignmentTypeComboBox.Items.Insert(0, "Individual Project");
-            this.assignmentTypeComboBox.Items.Insert(0, "Group Project");
-            this.assignmentTypeComboBox.Items.Insert(0, "Discussion Post");
-            this.assignmentTypeComboBox.Items.Insert(0, "Paper");
+            this.assignmentTypeComboBox.Items.Insert(1, "Test");
+            this.assignmentTypeComboBox.Items.Insert(2, "Individual Project");
+            this.assignmentTypeComboBox.Items.Insert(3, "Group Project");
+            this.assignmentTypeComboBox.Items.Insert(4, "Discussion Post");
+            this.assignmentTypeComboBox.Items.Insert(5, "Paper");
+        }
+
+        private void SetUpClassComboBox()
+        {
+            List<Course> currentCourses = this._courseController.GetCoursesByTeacherIDYearAndSemester(2, this.GetCurrentSemester(), DateTime.Now.Year);
+            foreach (Course course in currentCourses)
+            {
+                /*this.selectClassComboBox.Items.Add(course.Name + " - " + course.Prefix + " " + course.Number);*/
+                this.selectClassComboBox.Items.Add(course.Name);
+            }
+        }
+
+        private int GetCourseID()
+        {
+            /*Course selectedCourse = this.selectClassComboBox.GetItemText(this.selectClassComboBox.SelectedItem);
+            string courseName = selectedCourse*/;
+            /*Course selected = (Course)this.selectClassComboBox.SelectedItem;*/
+            /*MessageBox.Show(this._courseController.GetCourseByName(courseName));*/
+            /*MessageBox.Show(selected.Name);*/
+            return 4;
+        }
+
+        private string GetCurrentSemester()
+        {
+            string semester;
+
+            if (DateTime.Now.Month <= 4)
+            {
+                semester = "Spring";
+            }
+            else if (DateTime.Now.Month > 4 && DateTime.Now.Month < 8)
+            {
+                semester = "Summer";
+            }
+            else
+            {
+                semester = "Fall";
+            }
+
+            return semester;
+        }
+
+        private void CheckForParseErrors(bool parseable)
+        {
+            if (!parseable)
+            {
+                this.ShowInvalidErrorMessages();
+            }
+        }
+
+        private void ShowInvalidErrorMessages()
+        {
+            if (string.IsNullOrEmpty(this.selectClassComboBox.Text)) {
+                this.classErrorMessageLabel.Text = "Please select a course to create and assignment for.";
+                this.classErrorMessageLabel.ForeColor = Color.Red;
+            }
+
+            if (string.IsNullOrEmpty(this.assignmentTypeComboBox.Text))
+            {
+                this.assignmentTypeErrorMessageLabel.Text = "Please select an assignment type.";
+                this.assignmentTypeErrorMessageLabel.ForeColor = Color.Red;
+            }
+
+            if (!Decimal.TryParse(this.weightTextBox.Text.Trim(), out _) || Decimal.Parse(this.weightTextBox.Text.Trim()) < 0 || Decimal.Parse(this.weightTextBox.Text.Trim()) > 100 || string.IsNullOrEmpty(this.weightTextBox.Text))
+            {
+                this.weightErrorMessageLabel.Text = "Please enter a valid assignment weight. Ex: 7.0";
+                this.weightErrorMessageLabel.ForeColor = Color.Red;
+            }
+
+            if (string.IsNullOrEmpty(this.descriptionRichTextBox.Text))
+            {
+                this.descriptionErrorMessageLabel.Text = "Please input a brief assignment description.";
+                this.descriptionErrorMessageLabel.ForeColor = Color.Red;
+            }
+
+        }
+
+        private void HideInvalidErrorMessages()
+        {
+            this.classErrorMessageLabel.Text = "";
+            this.assignmentTypeErrorMessageLabel.Text = "";
+            this.weightErrorMessageLabel.Text = "";
+            this.descriptionErrorMessageLabel.Text = "";
+        }
+
+        private void ResetInputFields()
+        {
+            this.selectClassComboBox.SelectedItem = null;
+            this.assignmentTypeComboBox.SelectedItem = null;
+            this.weightTextBox.Clear();
+            this.descriptionRichTextBox.Clear();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            ResetInputFields();
+            this.HideInvalidErrorMessages();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this._courseID = this.GetCourseID();
+                Decimal weight;
+                this.CheckForParseErrors(Decimal.TryParse(this.weightTextBox.Text.Trim(), out weight));
+                var description = (this.assignmentTypeComboBox.Text + " - " + this.descriptionRichTextBox.Text.Trim());
+                this.ShowInvalidErrorMessages();
+                
+                if (string.IsNullOrEmpty(this.selectClassComboBox.Text) || string.IsNullOrEmpty(this.assignmentTypeComboBox.Text) /*|| !ValidationUtility.IsDecimal(this.weightTextBox.Text.Trim())*/ || !ValidationUtility.IsLessThan100(this.weightTextBox.Text.Trim()) || !ValidationUtility.IsGreaterThan0(this.weightTextBox.Text.Trim()) || string.IsNullOrEmpty(this.weightTextBox.Text.Trim()) || string.IsNullOrEmpty(this.descriptionRichTextBox.Text.Trim()))
+                {
+                    this.ShowInvalidErrorMessages();
+                    MessageBox.Show("Still registering error!");
+                } else
+                {
+                    if (this.ConfirmAssignment() == DialogResult.Yes)
+                    {
+                        Assignment newAssignment = new Assignment(-1, this._courseID, description, weight);
+                        bool success = this._assignmentController.AddNewAssignment(newAssignment);
+
+                        if (success)
+                        {
+                            MessageBox.Show("Assignment successfully saved!", "Assignment Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            this.ResetInputFields();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Assignment not added, there was an error.", "Assignment Not Saved", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+                }
+
+            }   
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private DialogResult ConfirmAssignment()
+        {
+            string selectedCourse = this.selectClassComboBox.GetItemText(this.selectClassComboBox.SelectedItem);
+            string type = this.assignmentTypeComboBox.Text.Trim();
+            DialogResult dialogResult = MessageBox.Show("Please confirm the following assignment details:" + "\n \n" + "   •  Course Name: " + selectedCourse + "\n \n" + "   •  Assignment Type: " + type + "\n \n" + "   •  Assignment Weight: " + this.weightTextBox.Text.Trim() + "\n  \n" + "   •  Description: " + this.descriptionRichTextBox.Text.Trim(), "Pending Assignment Details", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            return dialogResult;
+        }
+
+        private int GetTeacherID()
+        {
+            string teacherUsername = TeacherController.GetUsername();
+            Teacher currentTeacher = this._teacherController.GetTeacherByUsername(teacherUsername);
+            return currentTeacher.TeacherID;
+        }
+
+        private void ComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.HideInvalidErrorMessages();
         }
     }
 }
